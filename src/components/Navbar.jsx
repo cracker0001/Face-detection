c1fc12b2c1648ae3584a9a7f1ec78d587211530d
Prem Raj <pr20022008@gmail.com>
2024-06-10 03:02:54 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import './Navbar.css';
+const Navbar = () => {
+    return (
+        <div className="navbar">
+             <span className="face">Face-Detection</span>
+        </div>
+    )
+}
+
+export default Navbar
